e3e435053dec166d9967251daeb012f779560247
Nick Selvaggio <nick@segosolutions.com>
2023-05-23 14:39:41 -0400

    Added ability to extract rewards on an interval.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Validator Performance Aggregator. 
+# Validator Performance Aggregator. 
 
 Leveraging the Beaconcha.in api and a list of validators, we extract rewards every 100 epochs. This includes CC withdrawals and EC block proposal rewards.
 
@@ -7,3 +7,11 @@ To use this place a file named "validators.csv" in the data directory. The file 
 ```Index, Pubkey, Type, Node, Minipool address```
 
 We really only need the Index currently. 
+
+Some configuration options you can set in your environment: 
+
+### DEBUG = true || false  
+This will output additional details regarding the various API calls and results from those calls.
+
+### EPOCH_INTERVAL = <integer>
+This will be the number of epochs to wait until trying to extract reward data. Beaconcha.in uses 100.
--- index.js
+++ index.js
@@ -3,59 +3,46 @@
  */
 
 // Import our tools.
-const { readValidators, getWithdrawals, getLatestEpoch, getProposals } = require('./lib/rewardExtractor.js');
+const { readValidators, extractRewards, getLatestEpoch } = require('./lib/rewardExtractor.js');
 const { chunkArray } = require('./lib/utils.js');
 
+// If set to true then additional output will be printed to the console.
+const debug = process.env.DEBUG;
 
-// Immediatly invoked async function
+// Immediately invoked async function
 (async () => {
 
     const validators = readValidators();
-    const chunks = chunkArray(validators, 100); // Beaconcha.in api allows 100 validators per request.
-
-    // Build an array of CSV strings for each chunk.
-    const validatorChunks = chunks.map((chunk) => {
-        let validatorList = '';
-        // For each validators get the withdrawals
-        for (let i = 0; i < chunk.length; i++) {
-            // Build a comma seperated list of validators.
-            validatorList += chunk[i].index + ',';
-        }
-
-        // Remove the last comma
-        validatorList = validatorList.slice(0, -1);
-        return validatorList;
-    });
+    const validatorChunks = chunkArray(validators, 100); // Beaconcha.in api allows 100 validators per request.
 
     const epoch = await getLatestEpoch();
 
-    console.log('Latest epoch: ' + epoch.data.epoch);
-
-    // Loop through all the validatorChunks
-    for (let i = 0; i < validatorChunks.length; i++) {
-
-        // Lets get all withdrawals for the last 100 epochs
-        const withdrawals = await getWithdrawals(validatorChunks[i], epoch.data.epoch);
-
-        console.log("Widthdrawals:\n ");
-        console.log(withdrawals);
-
-        // Get proposals for each validator
-        console.log("Proposals:\n ");
-        const proposals = await getProposals(validatorChunks[i], epoch.data.epoch);
-        console.log(proposals);
-
-        // Loop through each proposal and grab the mev. 
-        // Endpoint: https://boost-relay.flashbots.net/relay/v1/data/bidtraces/proposer_payload_delivered?block_number=<blocknumber>
-        let mev_total = 0;
-        proposals.data.forEach(async (proposal) => {
-            console.log("We have a proposal.")
-            // Fetch the mev from the boost-relay.
-            const url = 'https://boost-relay.flashbots.net/relay/v1/data/bidtraces/proposer_payload_delivered?block_number=' + proposal.exec_block_number;
-            console.log(url)
-            const mev = await fetch(url)
-            const mev_data = await mev.json();
-            console.log(mev_data)
-        })
+    let epochInterval;
+    if(process.env.EPOCH_INTERVAL) {
+        epochInterval = process.env.EPOCH_INTERVAL;
     }
+    else {
+        epochInterval = 100;
+    }
+
+    console.log("\n---------------------------\nLIBC Validator Monitor\n---------------------------\n");
+    console.log('Checking every ' + epochInterval + ' epochs for new rewards.\n');
+
+    // Every 1 minute check for new epoch.
+    setInterval(async () => {
+        console.log('Checking for new epoch...')
+        const latestEpoch = await getLatestEpoch();
+        // If the latest epoch is 100 epochs ahead of the current epoch, then we have a new epoch.
+        console.log('Current epoch: ' + latestEpoch.data.epoch);
+        // Add 100 to the epoch interval to account for the current epoch as an integer
+        let nextCheck = parseInt(latestEpoch.data.epoch) + parseInt(epochInterval);
+        console.log('Next rewards check Epoch: ' + nextCheck + '\n')
+        if (latestEpoch.data.epoch > nextCheck) {
+            if(debug) {
+                console.log('Checking for new rewards');
+            }
+            const rewards = extractRewards(validatorChunks, epoch.data.epoch);
+        }
+    }, 60000);
+    
 })();
