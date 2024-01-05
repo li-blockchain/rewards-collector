5c047103b6fcf38e4b6176e040313f7c50b64e85
Nick Selvaggio <nick@segosolutions.com>
2024-01-04 23:01:22 -0500

    Added support for discord bot messaging / logging

--- backfiller.js
+++ backfiller.js
@@ -12,63 +12,91 @@ const fs = require('fs');
 // If set to true then additional output will be printed to the console.
 const debug = process.env.DEBUG;
 
-// Immediately invoked async function
-(async () => {
+const { Client, GatewayIntentBits, Events } = require('discord.js');
 
-    // We want to track how long this process takes.
-    const start = Date.now();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-    // Read validators and chunk them up. Beaconcha.in api allows 100 validators per request.
-    const validators = readValidators();
-    const validatorChunks = chunkArray(validators, 100);
+client.login(process.env.DISCORD_KEY);
 
-    // If the data/.lastepoch file exists then we want to start from there.
-    // This is useful if the process is interrupted.
-    // If the file does not exist then we will start from the EPOCH_START env variable.
-
-    if(fs.existsSync('./data/.lastepoch')) {
-        const lastEpoch = fs.readFileSync('./data/.lastepoch', 'utf8');
-
-        // validate lastEpoch is a number.
-        if(isNaN(lastEpoch)) {
-            console.log('Error: .lastepoch file parse error. Not a valid epoch number.');
-            process.exit();
+async function sendMessageToChannel(channelId, message) {
+    try {
+        const channel = await client.channels.fetch(channelId);
+        if (!channel) {
+            console.error(`Channel not found for ID: ${channelId}`);
+            return;
         }
-
-        console.log('Using .lastepoch file: ' + lastEpoch);
-        process.env.EPOCH_START = lastEpoch;
+        await channel.send(message);
+    } catch (error) {
+        console.error(`Error sending message to channel ${channelId}:`, error);
     }
+}
 
-    // Get our starting epoch. 
-    const epoch = process.env.EPOCH_START
+client.once('ready', () => {
+    console.log('Discord bot is ready!');
+    // Immediately invoked async function
+    (async () => {
 
-    console.log("\n---------------------------\nLIBC Validator Backfill\n---------------------------\n");
-    console.log("Starting on epoch: " + epoch + "\n");
+        // We want to track how long this process takes.
+        const start = Date.now();
 
-    // First run.
-    const rewards = await extractRewards(validatorChunks, epoch);
-    saveRewards(rewards);
+        await sendMessageToChannel(process.env.DISCORD_CHANNEL, 'Getting some reward data.');
 
-    // Every 1 minute check for new epoch.
-    let nextEpoch = epoch;
-    setInterval(async () => {
-        const currentEpoch = await getLatestEpoch(0);
-        console.log('Current epoch: ' + currentEpoch.data.epoch);
-        nextEpoch = parseInt(nextEpoch) + 100;
-        if(nextEpoch > currentEpoch.data.epoch && currentEpoch.data.epoch > 0) {
-            // Save nextEpoch to data/.lastepoch file
-            fs.writeFileSync('./data/.lastepoch', String(nextEpoch));
+        // Read validators and chunk them up. Beaconcha.in api allows 100 validators per request.
+        const validators = readValidators();
+        const validatorChunks = chunkArray(validators, 100);
 
-            console.log('Backfill Complete on Epoch ' + (nextEpoch-100) + '!');
-            console.log('Total time: ' + ((Date.now() - start) / 1000) + ' seconds');
-            process.exit();
+        // If the data/.lastepoch file exists then we want to start from there.
+        // This is useful if the process is interrupted.
+        // If the file does not exist then we will start from the EPOCH_START env variable.
+
+        if(fs.existsSync('./data/.lastepoch')) {
+            const lastEpoch = fs.readFileSync('./data/.lastepoch', 'utf8');
+
+            // validate lastEpoch is a number.
+            if(isNaN(lastEpoch)) {
+                console.log('Error: .lastepoch file parse error. Not a valid epoch number.');
+                process.exit();
+            }
+
+            console.log('Using .lastepoch file: ' + lastEpoch);
+            process.env.EPOCH_START = lastEpoch;
         }
 
-        console.log('Collecting rewards for: ' + nextEpoch + '\n')
+        // Get our starting epoch. 
+        const epoch = process.env.EPOCH_START
 
-        const rewards = await extractRewards(validatorChunks, nextEpoch);
-        await saveRewards(rewards);
+        console.log("\n---------------------------\nLIBC Validator Backfill\n---------------------------\n");
+        console.log("Starting on epoch: " + epoch + "\n");
 
-    }, 10000);
+        // First run.
+        //const rewards = await extractRewards(validatorChunks, epoch);
+        //saveRewards(rewards);
 
-})();
+        // Every 1 minute check for new epoch.
+        let nextEpoch = epoch;
+        setInterval(async () => {
+            const currentEpoch = await getLatestEpoch(0);
+            console.log('Current epoch: ' + currentEpoch.data.epoch);
+            nextEpoch = parseInt(nextEpoch) + 100;
+            if(nextEpoch > currentEpoch.data.epoch && currentEpoch.data.epoch > 0) {
+                // Save nextEpoch to data/.lastepoch file
+                fs.writeFileSync('./data/.lastepoch', String(nextEpoch));
+
+                console.log('Backfill Complete on Epoch ' + (nextEpoch-100) + '!');
+                console.log('Total time: ' + ((Date.now() - start) / 1000) + ' seconds');
+
+                //await sendMessageToChannel('1055560295898152970', 'Reward extraction complete.');
+                await sendMessageToChannel(process.env.DISCORD_CHANNEL, 'Backfill Complete on Epoch ' + (nextEpoch-100) + '!');
+                await sendMessageToChannel(process.env.DISCORD_CHANNEL, 'Total time: ' + ((Date.now() - start) / 1000) + ' seconds');
+                process.exit();
+            }
+
+            console.log('Collecting rewards for: ' + nextEpoch + '\n')
+
+            const rewards = await extractRewards(validatorChunks, nextEpoch);
+            await saveRewards(rewards);
+
+        }, 10000);
+
+    })();
+});
